d4255aac5118ef2b4e27007c798dfc417c5b0458
nakzgeo69 <nakzgeo@gmail.com>
2026-07-12 08:07:34 +0800

    Update enhance.js

--- commands/enhance.js
+++ commands/enhance.js
@@ -67,7 +67,7 @@ module.exports = {
 
         if (data.author) {
           await sendMessage(senderId, {
-            text: `${data.author}`
+            text: ``
           }, token);
         }
 
